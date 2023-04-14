0cdff7a53d712856a0cf017ff67472b7139f4d21
Pedro Pérez Montero <127769094+KuroiiRyuu@users.noreply.github.com>
2023-04-14 12:44:57 +0200

    collatz comentarios

--- collatzEnNumerosEspecificos.py
+++ collatzEnNumerosEspecificos.py
@@ -1,11 +1,7 @@
 #Variables
 lista = []
-
 a = "a"
-
-c = 0
-
-b = 0
+c = 0 #Contador
 
 #Explicacion del codigo
 print("Este programa ejecutara todas las secuencias de collatz que introduzacas")
@@ -15,12 +11,9 @@ print("Una vez hayas acabado pondras un asterisco '#' y se te mostraran todas la
 
 #Bucle para confirmar si se ha introducido el #
 while a != "#":
-
     a = input()
-
     c += 1
-
-    lista.append(a)
+    lista.append(a) #Añadir los numeros a una lista para luego poder realizarles la conjetura de collatz
 
 lista.pop(-1) #Eliminar el # de la lista de elementos
 
